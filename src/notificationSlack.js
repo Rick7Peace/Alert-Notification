9514cccca8b8690ae56ad27d45ed0b5d223be9a5
ricardo marmolejo <marmolejo.ricardo@gmail.com>
2025-04-06 16:47:34 -0400

    completed slack webhook

--- src/notificationSlack.js
+++ src/notificationSlack.js
@@ -1,11 +1,29 @@
-const { default: axios } = require('axios');
-
-const handler = async (event) => {
+export const handler = async (event) => {
   try {
     for (const record of event.Records) {
       try {
         console.log(record.body);
         //need to send record.body to slack url using axios
+        const slackWebhookUrl = 'https://hooks.slack.com/services/T01JZK2Q6M3/B01JZK2Q6M3/8LjH6s5s3fBQr3F2p5tH2Z0';  // Replace with your actual Slack webhook URL
+
+        // Create the payload to send to Slack
+        const slackMessage = {
+          text: record.body, // You can adjust this to include specific fields from record.body if needed
+        };
+
+        // Send the message to Slack
+        const response = await fetch(slackWebhookUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(slackMessage),
+        });
+
+        if (!response.ok){
+          throw new Error('Error sending message to slack: ' + response.statusText);
+         }
+        console.log('Message sent to Slack successfully');
       } catch (e) {
         console.error(e)
         throw e;
@@ -17,6 +35,3 @@ const handler = async (event) => {
     }
   }
 
-  module.exports = {
-    handler,
-  };
